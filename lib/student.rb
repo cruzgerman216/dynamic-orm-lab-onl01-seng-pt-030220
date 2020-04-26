eddb28d69ef5ce807aab0b92bfbf84896c51021a
German Cruz <cruzgerman216@yahoo.com>
2020-04-26 16:59:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ require 'interactive_record.rb'
 class Student < InteractiveRecord
 
   def initialize(options={})
-    options.each do |property, value| 
+    options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
